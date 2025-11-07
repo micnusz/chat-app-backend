2ed1d5982c584f7f16a677a3cd9de98bd49520e1
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-11-07 23:50:21 +0100

    Updated securityConfig

--- src/main/java/com/micnusz/chat/config/SecurityConfig.java
+++ src/main/java/com/micnusz/chat/config/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/api/users/login",
                     "/api/users/register",
-                    "/api/users/refresh"
+                    "/api/users/refresh",
+                            "/health"    
                 ).permitAll()
                 .requestMatchers("/api/users/me").authenticated()
                 .anyRequest().authenticated()
