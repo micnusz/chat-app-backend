775556bcd4fd359a8a2f19dbd69d0bf63b67a1d8
Michal <michalnuszkiewiczbusiness@gmail.com>
2025-11-09 13:38:08 +0100

    Minor webscoketconfig changes

--- src/main/java/com/micnusz/chat/config/WebSocketConfig.java
+++ src/main/java/com/micnusz/chat/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler, "/chat")
+        registry.addHandler(chatWebSocketHandler, "/chat/**")
                 .setAllowedOrigins(
                     "https://chatapp.micnusz.xyz",
                     "http://localhost:3000"
